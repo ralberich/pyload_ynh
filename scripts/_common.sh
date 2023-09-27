9199004b078c9db820deb1d71a3bfc8afdee0e24
orhtej2 <2871798+orhtej2@users.noreply.github.com>
2023-09-27 11:09:22 +0200

    Add python3-dev to fix Bookworm compatibility

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="python3-cryptography python3-pycurl python3-venv libcurl4-openssl-dev libssl-dev"
+pkg_dependencies="python3-cryptography python3-pycurl python3-venv libcurl4-openssl-dev libssl-dev python3-dev"
 
 #=================================================
 # PERSONAL HELPERS
